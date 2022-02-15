ade7de8009fc84c8800fe36bcb0a84a532fb0757
Raihan Taher <rayhan.taher@gmail.com>
2022-02-16 02:15:14 +0600

    habijabi

---
+++ 771.py
@@ -0,0 +1,23 @@
+jewels = "aA"
+stones = "aAAbbbb"
+
+flag = {}
+cnt = 0
+for char in jewels:
+    flag[char] = 1
+
+for char in stones:
+    # if flag[char] == 1:
+    print(flag[char])
+
+
+
+int sum(int a, int b, int c){
+    return a+b+c;
+}
+
+int sum(int a,int b){
+    return a+b;
+}
+
+

aaf50a04c9090cf859fb3890bbf03e92cc3a6b01
Raihan Taher <rayhan.taher@gmail.com>
2022-02-22 03:57:21 +0600

    53. max subarray

--- 771.cpp
+++ 771.cpp
@@ -5,17 +5,15 @@ int main()
 {
     int x[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4}, sz = 9;
     int sum = 0;
-    int mx = x[0];
+    int mx = INT_MIN;
     for (int i = 0; i < sz; i++)
     {
-        if (mx + x[i] > mx)
-        {
-            mx = mx + x[i];
-        }
-        else if (x[i] > mx)
-        {
-            mx = x[i];
-        }
+        sum += x[i];
+        if (sum > mx)
+            mx = sum;
+        if (sum < 0)
+            sum = 0;
     }
+    cout << mx << endl;
     return 0;
 }

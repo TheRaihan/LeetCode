1138bdec071e2f16aab3e66f3a40e99782a27989
Raihan Taher <rayhan.taher@gmail.com>
2022-02-22 04:09:34 +0600

    Update 771.cpp

--- 771.cpp
+++ 771.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main()
 {
-    int x[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4}, sz = 9;
-    int sum = 0;
-    int mx = INT_MIN;
-    for (int i = 0; i < sz; i++)
-    {
-        sum += x[i];
-        if (sum > mx)
-            mx = sum;
-        if (sum < 0)
-            sum = 0;
-    }
-    cout << mx << endl;
-    return 0;
+    // int x[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4}, sz = 9;
+    // int sum = 0;
+    // int mx = INT_MIN;
+    // for (int i = 0; i < sz; i++)
+    // {
+    //     sum += x[i];
+    //     if (sum > mx)
+    //         mx = sum;
+    //     if (sum < 0)
+    //         sum = 0;
+    // }
+    // cout << mx << endl;
+    // return 0;
 }

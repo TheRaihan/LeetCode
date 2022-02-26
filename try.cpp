5b7e1d9c7af09d794c5985e9da11b66b27047752
Raihan Taher <rayhan.taher@gmail.com>
2022-02-26 19:16:09 +0600

    3Sum in cpp

--- try.cpp
+++ try.cpp
@@ -1,49 +1,66 @@
 // CPP program to initialize a vector like
 // an array.
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     vector<int> vect, ans;
+    set<vector<int> > unique_results;
     int mul = 1, zero = 0, flag = 0;
 
     vect.push_back(-1);
+    vect.push_back(2);
+    vect.push_back(0);
     vect.push_back(1);
-    vect.push_back(0);
-    vect.push_back(-3);
-    vect.push_back(0);
-    for (int i = 0; i < vect.size(); i++)
+    vect.push_back(-1);
+    vect.push_back(4);
+
+    sort(vect.begin(), vect.end());
+    set<vector<int>, greater<int> >::iterator itr;
+    int sz = vect.size(), k, sum = 0;
+
+    for (int i = 0; i < sz; i++)
     {
-        if (vect[i] == 0)
+        k = sz - 1;
+        for (int j = i + 1; j < sz; )
         {
-            zero++;
-            flag = i;
-            continue;
+            if (j == k)
+                break;
+            sum = vect[i] + vect[j] + vect[k];
+            if (sum == 0)
+            {
+                cout << vect[i] << " " << vect[j] << " " << vect[k--] << " " << endl;
+            }
+            else if (sum > 0)
+            {
+                k--;
+            }
+            else
+                j++;
         }
-        mul = mul * vect[i];
     }
 
-    for (int i = 0; i < vect.size(); i++)
-    {
-        if (zero > 0)
-        {
-            ans.push_back(0);
-        }
-        else
-            ans.push_back(mul / vect[i]);
-    }
+    // vector<vector<int> > results;
 
-    if (zero == 1)
-        ans[flag] = mul;
+    // std::copy(
+    //     unique_results.begin(), unique_results.end(),
+    //     std::back_inserter(results));
 
-    cout << mul << endl;
+    // for (std::vector<vector<int> >::iterator it = results.begin(); it != results.end(); ++it)
+    // {
+    //     for (vector<unsigned char>::iterator it1 = (*it).begin(); it1 != (*it).end(); ++it1)
+    //     {
+    //         cout << *it1 << endl;
+    //     }
+    // }
 
-    for (int i = 0; i < vect.size(); i++)
-    {
-        cout << ans[i] << " ";
-    }
-
+    // for (const auto &v : results)
+    // {
+    //     for (auto i : v)
+    //     {
+    //         cout << i << endl;
+    //     }
+    // }
     return 0;
 }

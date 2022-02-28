9fecf6a7ee5b9a1e84b5b76e54c6ee7ad1ffffd8
Raihan Taher <rayhan.taher@gmail.com>
2022-03-01 03:09:11 +0600

    asd

---
+++ 15.py
@@ -0,0 +1,15 @@
+O(n) - Complexity
+SDLC
+SQL
+DFS/BFS
+database
+
+O(1)
+O(logn)
+O(n)
+O(nlogn)
+O(n^2)
+O(n^3)
+O(2^n)
+O(n^n)
+
--- try.cpp
+++ try.cpp
@@ -5,41 +5,60 @@ using namespace std;
 
 int main()
 {
-    vector<int> vect, ans;
-    set<vector<int> > unique_results;
-    int mul = 1, zero = 0, flag = 0;
+    vector<int> nums; // 1 2 4 5 0 -- 3
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(4);
+    nums.push_back(5);
+    nums.push_back(0);
 
-    vect.push_back(-1);
-    vect.push_back(2);
-    vect.push_back(0);
-    vect.push_back(1);
-    vect.push_back(-1);
-    vect.push_back(4);
+    int result = nums.size();
+    int i = 0;
 
-    sort(vect.begin(), vect.end());
-    set<vector<int>, greater<int> >::iterator itr;
-    int sz = vect.size(), k, sum = 0;
-
-    for (int i = 0; i < sz; i++)
+    for (int num : nums)
     {
-        k = sz - 1;
-        for (int j = i + 1; j < sz;)
-        {
-            if (j == k)
-                break;
-            sum = vect[i] + vect[j] + vect[k];
-            if (sum == 0)
-            {
-                cout << vect[i] << " " << vect[j] << " " << vect[k--] << " " << endl;
-            }
-            else if (sum > 0)
-            {
-                k--;
-            }
-            else
-                j++;
-        }
+        result ^= num;
+        result ^= i;
+        i++;
     }
+
+    cout << result << endl;
+
+    // vector<int> vect, ans;
+    // set<vector<int> > unique_results;
+    // int mul = 1, zero = 0, flag = 0;
+
+    // vect.push_back(-1);
+    // vect.push_back(2);
+    // vect.push_back(0);
+    // vect.push_back(1);
+    // vect.push_back(-1);
+    // vect.push_back(4);
+
+    // sort(vect.begin(), vect.end());
+    // set<vector<int>, greater<int> >::iterator itr;
+    // int sz = vect.size(), k, sum = 0;
+
+    // for (int i = 0; i < sz; i++)
+    // {
+    //     k = sz - 1;
+    //     for (int j = i + 1; j < sz;)
+    //     {
+    //         if (j == k)
+    //             break;
+    //         sum = vect[i] + vect[j] + vect[k];
+    //         if (sum == 0)
+    //         {
+    //             cout << vect[i] << " " << vect[j] << " " << vect[k--] << " " << endl;
+    //         }
+    //         else if (sum > 0)
+    //         {
+    //             k--;
+    //         }
+    //         else
+    //             j++;
+    //     }
+    // }
 
     // vector<vector<int> > results;
 

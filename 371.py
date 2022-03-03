23e2d44e446176a7246cf7b1b29d9d4e3d303a60
Raihan Taher <rayhan.taher@gmail.com>
2022-03-04 03:03:15 +0600

    python edited

--- 371.py
+++ 371.py
@@ -1,8 +1,15 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        c = 0
-        while(b!=0):
-            c = a&b
-            a = a^b
-            b = c << 1
-        return a
+        mask = 0xffffffff
+        while b:
+		    sum = (a^b) & mask
+			carry = ((a&b)<<1) & mask
+            a = sum
+			b = carry
+
+		if (a>>31) & 1: # If a is negative in 32 bits sense
+			return ~(a^mask) #What the first line means is that if what would be the sign bit in a signed 32 bit integer is set then the number is negative. Shift the sign bit over 31 spots then AND it with a 1 to see if its set.
+# What the second line does it returns the two's complement of that number. He gives a solid explanation of how ~(a^mask) is derived to be the two's complement. Normally we'd just flip all the bits and add 1 but we can't do that here.
+		return a
+
+
--- 771.cpp
+++ 771.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-    int a = 5, b =7;
-    long long c = 0 ;
-    
-    while(b!=0)
+    int a = 5, b = 7;
+    long long c = 0;
+
+    while (b != 0)
     {
-        c = a&b;
-        a = a^b;
+        c = a & b;
+        a = a ^ b;
         b = (unsigned int)c << 1;
     }
     cout << a << endl;

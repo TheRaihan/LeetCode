8e8f80b248f43e185dcb76b14c89e436d86abaec
Raihan Taher <rayhan.taher@gmail.com>
2022-03-08 02:57:45 +0600

    Update try.cpp

--- try.cpp
+++ try.cpp
@@ -5,11 +5,31 @@ using namespace std;
 
 int main()
 {
-    vector<int> nums{ 4,1,2,1,2 };
-    int ans = 0;
-    for (int x : nums)
-        ans = ans ^ x;
+    vector<int> list1{1, 2, 3}, list2{4, 5, 6}, list3;
+    int i = 0, j = 0,flag1=0,flag2=0;
+    while (list3.size() < list1.size() + list2.size())
+    {
+        if(flag1 == 1 && flag2 == 1) break;
+        if (list1[i] <= list2[j] && list3.size() < list1.size() + list2.size())
+        {
+            list3.push_back(list1[i++]);
+            if(i == list1.size()){
+                i--;
+                flag1 = 1;
+            }
+        }
+        if (list2[j] <= list1[i] && list3.size() < list1.size() + list2.size())
+        {
+            list3.push_back(list2[j++]);
+            if(j == list2.size()){
+                j--;
+                flag2 = 1;
+            }
+        }
+    }
+    cout << i << " " << j << endl;
+    for (int x : list3)
+        cout << x << " ";
 
-    cout << ans << endl;
     return 0;
 }

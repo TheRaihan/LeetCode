0df2e80c2d20c723f3a7c49da97d00be4cdd2cb2
Raihan Taher <rayhan.taher@gmail.com>
2022-03-18 20:52:51 +0600

    smallest subsequence

---
+++ 1081.py
@@ -0,0 +1,16 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        cnt= Counter(s)
+        visited = set()
+        stack = []
+        
+        for c in s:
+            cnt[c]-=1
+            if c not in visited:
+                while stack and stack[-1] > c and cnt[stack[-1]] > 0:
+                    visited.remove(stack.pop())
+                visited.add(c)
+                stack.append(c)
+                
+        return "".join(stack)
+        

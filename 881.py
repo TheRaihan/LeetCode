76e5901777b9d5c9a28fc3b287e74ac0a773a2c2
Raihan Taher <rayhan.taher@gmail.com>
2022-03-25 03:18:42 +0600

    Update 881.py

--- 881.py
+++ 881.py
@@ -3,20 +3,13 @@ public:
     int numRescueBoats(vector<int>& people, int limit) {
         int cnt=0;
         sort(people.begin(),people.end());
-        for(int i=0,j=people.size()-1; i<=j; j--)
+        
+        for(int i=0,j=people.size()-1; i<=j ; j--)
         {
-            if(i==j){
-                cnt++;
-                break;
-            }
-
-            if(people[i]+people[j] <= limit){
-                people[i] = 0;
-                i++;
-            }
-            
-            people[j] = 0;
             cnt++;
+            if(i==j) break;
+            if(people[i]+people[j] <= limit)
+                i++;
         }
         return cnt;
     }

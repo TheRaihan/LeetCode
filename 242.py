287d588b62033e053862d60608f48bbd83e46d8e
Raihan Taher <rayhan.taher@gmail.com>
2022-03-29 03:31:52 +0600

    apprch 2

--- 242.py
+++ 242.py
@@ -1,6 +1,28 @@
 
+#1
 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        int cnt[26]={0};
+        
+        if(s.size() != t.size()) return false;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            cnt[s[i] - 'a']++;
+            cnt[t[i] - 'a']--;
+        }
+        
+        for(int i=0;i<26;i++)
+            if(cnt[i])
+                return false;
 
+    
+        return true;
+    }
+};
 
 #2
 sort(s.begin(), s.end());

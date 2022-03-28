e29751b2bd2170e4237b87a41277c76ef871412d
Raihan Taher <rayhan.taher@gmail.com>
2022-03-29 03:09:39 +0600

    abar kora lagbe

--- try2.cpp
+++ try2.cpp
@@ -28,6 +28,22 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int n=mat[0].size();
+        for(int i=0; i<mat.size(); i++){
+            mat[i].push_back(i);
+        }
+        sort(mat.begin(), mat.end());
+        vector<int> ans(k);
+        for(int i=0; i<k; i++){
+            ans[i]=mat[i][n];            
+        }
+        return ans;
+    }
+};
 /**
  * Your FreqStack object will be instantiated and called as such:
  * FreqStack* obj = new FreqStack();

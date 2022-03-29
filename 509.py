a5e79eb4cfe5ef181d896a822b16503d0c8b6b6d
Raihan Taher <rayhan.taher@gmail.com>
2022-03-30 04:55:28 +0600

    python

--- 509.py
+++ 509.py
@@ -1,8 +1,5 @@
-class Solution {
-public:
-    int fib(int n) {
-        if(n<=1) return n;
-        
-        return fib(n-1)+fib(n-2);
-    }
-};
+class Solution:
+    def fib(self, n: int) -> int:
+        if n<=1: 
+            return n
+        return self.fib(n-1) + self.fib(n-2)

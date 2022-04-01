de4106cf3aecfe856402340f9aaf3accd4392bb9
Raihan Taher <rayhan.taher@gmail.com>
2022-04-02 03:52:45 +0600

    python

--- 557.py
+++ 557.py
@@ -1,19 +1,20 @@
-                    res.append(x);
-#include <string>
+# python
+def reverseWords(self, s):
+    return ' '.join(x[::-1] for x in s.split())
 
+
+# C++
 class Solution {
 public:
     string reverseWords(string s) {
         stack<char> st;
-        string res;
-        string x;
+        string res,x;
         for(int i=0;i<s.size();i++)
         {
             if(s[i] != ' ')
                 st.push(s[i]);
     
             else{
-                
                 while(!st.empty())
                 {
                     x = st.top();
@@ -23,12 +24,14 @@ public:
                 res.append(" ");
             }
         }
+
         while(!st.empty())
         {
             x = st.top();
             st.pop();
             res.append(x);
         }
+
         return res;
     }
 };

750d8916a6524d060d29273487396bf66efb00b4
Raihan Taher <rayhan.taher@gmail.com>
2022-04-03 03:26:57 +0600

    Update 183.sql

--- 183.sql
+++ 183.sql
@@ -5,3 +5,6 @@ WHERE Orders.CustomerId is NULL
 
 SELECT Name as Customers from Customers 
 WHERE Id NOT IN (SELECT CustomerId from Orders)
+
+SELECT Name as Customers from Customers
+WHERE NOT EXISTS (SELECT * FROM Orders WHERE Customers.Id = Orders.CustomerId)

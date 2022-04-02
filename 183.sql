7e408911d254c775ecd5ac0832b166a70a7fa5e8
Raihan Taher <rayhan.taher@gmail.com>
2022-04-03 03:26:42 +0600

    Update 183.sql

--- 183.sql
+++ 183.sql
@@ -1,3 +1,7 @@
 SELECT name as Customers FROM Customers 
 LEFT JOIN Orders ON Customers.id = Orders.CustomerId 
 WHERE Orders.CustomerId is NULL
+
+
+SELECT Name as Customers from Customers 
+WHERE Id NOT IN (SELECT CustomerId from Orders)

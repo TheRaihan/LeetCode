a0c654d71fbf2337a143c9a678fd76996ffd39bc
Raihan Taher <rayhan.taher@gmail.com>
2022-04-03 03:11:48 +0600

    Create 989.js

---
+++ 989.js
@@ -0,0 +1,13 @@
+var addToArrayForm = function (A, K) {
+    let flag = A.length - 1
+    while (K) {
+        if (flag < 0) {
+            A.unshift(K % 10)
+        } else {
+            K += A[flag]
+            A[flag--] = K % 10
+        }
+        K = Math.floor(K / 10)
+    }
+    return A
+}

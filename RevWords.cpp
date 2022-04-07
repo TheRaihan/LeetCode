94408c0d15b89cabb2965f8ac4bd53fc5b282b33
Raihan Taher <rayhan.taher@gmail.com>
2022-04-08 04:57:26 +0600

    Create RevWords.cpp

---
+++ RevWords.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    stack<char> st;
+    string x = "Mountains are calling";
+
+    for(int i=0;i<x.size();i++)
+    {
+        if(x[i] != ' ' && i != x.size()-1)
+            st.push(x[i]);
+
+        else{
+            while(!st.empty())
+            {
+                cout << st.top();
+                st.pop();
+            }
+            if(i != x.size()-1)
+                cout << " ";
+        }
+
+    }
+        cout << endl;
+    return 0;
+}

14c7fdf81d75f0ad04dfd98c1823d582ee4415b0
Raihan Taher <rayhan.taher@gmail.com>
2022-04-13 03:49:33 +0600

    Update 226.py

--- 226.py
+++ 226.py
@@ -1,13 +1,13 @@
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {
-        if(!root)
-            return NULL;
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
         
-        swap(root->left, root->right);
-        invertTree(root->left);
-        invertTree(root->right);
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
         
-        return root;
-    }
-};
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
